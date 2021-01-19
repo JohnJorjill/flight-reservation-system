37a9bcac439a2833bfb501e64a792cd925a90b42
John Jorjill <orgil9506@gmail.com>
2021-01-19 17:22:01 -0600

    Added class variable to the ReservationSystemRepositoryImpl class

--- src/main/java/edu/miu/cs/cs401/project/repository/ReservationSystemRepositoryImpl.java
+++ src/main/java/edu/miu/cs/cs401/project/repository/ReservationSystemRepositoryImpl.java
@@ -1,19 +1,33 @@
 package edu.miu.cs.cs401.project.repository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import edu.miu.cs.cs401.project.domain.Address;
+import edu.miu.cs.cs401.project.domain.Agent;
 import edu.miu.cs.cs401.project.domain.Airline;
 import edu.miu.cs.cs401.project.domain.Airport;
+import edu.miu.cs.cs401.project.domain.Crew;
 import edu.miu.cs.cs401.project.domain.Flight;
+import edu.miu.cs.cs401.project.domain.FlightInstance;
 import edu.miu.cs.cs401.project.domain.Passenger;
+import edu.miu.cs.cs401.project.domain.Pilot;
 import edu.miu.cs.cs401.project.domain.Reservation;
 
 public class ReservationSystemRepositoryImpl implements ReservationSystemRepository {
 	
-	private Map<String, Airport> airports = new HashMap<>();
+	// Added class variables by Orgil
+	private List <Airport> airports = new ArrayList<>();
+	private Map<Integer, Passenger> passengers = new HashMap<>();
+	private Map<String, Airline> airlines= new HashMap<>();
+    private List <FlightInstance> allFlight = new ArrayList<>();
+
+    private Map<Integer, Pilot> pilots= new HashMap<>();
+	private Map<Integer, Crew> crews= new HashMap<>();
+	private Map<Integer, Agent> agents= new HashMap<>();
 
 	ReservationSystemRepositoryImpl() {
 		super();
@@ -36,7 +50,7 @@ public class ReservationSystemRepositoryImpl implements ReservationSystemReposit
 	}
 
 	@Override
-	public Collection<Airport> findAllAirports() {
+	public List<Airport> findAllAirports() {
 		return airports.values();
 	}
 
