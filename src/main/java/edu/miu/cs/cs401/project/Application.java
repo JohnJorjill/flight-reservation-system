e9cf684bf4017a557dfdb702dd9b374b23bb2fcb
John Jorjill <orgil9506@gmail.com>
2021-01-19 15:09:28 -0600

    Cleaned up code - Orgil

--- src/main/java/edu/miu/cs/cs401/project/Application.java
+++ src/main/java/edu/miu/cs/cs401/project/Application.java
@@ -4,19 +4,14 @@ import edu.miu.cs.cs401.project.service.ReservationSystemService;
 import edu.miu.cs.cs401.project.service.ServiceFactory;
 
 public class Application {
-//hello
 
 	public static void main(String[] args) {
 		System.out.println("Airline Reservation System");
 
 		ReservationSystemService service = ServiceFactory.getReservationSystemService();
 		System.out.println(service.findAllAirports());
-		
-		//hello everyone 
+	
 		System.out.println(service.findAirportByAirportCode("CID"));
-		System.out.println(service.findAirportByAirportCode("CID"));
-		System.out.println(service.findAirportByAirportCode("CID"));
-		// orgil
 	}
 
 }
