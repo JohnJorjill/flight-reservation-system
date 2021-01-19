b6e185fc57a08ab3b57805a1d2c7ac0fe0951414
Stole <stole.t@outlook.com>
2021-01-19 17:45:55 -0600

    Airport fully implemented

--- src/main/java/edu/miu/cs/cs401/project/domain/Airport.java
+++ src/main/java/edu/miu/cs/cs401/project/domain/Airport.java
@@ -1,16 +1,18 @@
 package edu.miu.cs.cs401.project.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Airport {
-	
+
 	private final String id;
-	
+
 	private String code;
-	
 	private String name;
-	
 	private Address address;
+	private List<Airline> arrivalAirlines;
+	private List<Airline> departureAirlines;
 
 	public Airport(String code, String name, Address address) {
 		super();
@@ -18,26 +20,16 @@ public class Airport {
 		this.name = name;
 		this.address = address;
 		this.id = UUID.randomUUID().toString();
+		arrivalAirlines = new ArrayList<>();
+		departureAirlines = new ArrayList<>();
 	}
 
-	public String getId() {
-		return id;
+	public void addArrivalAirline(Airline airline) {
+		arrivalAirlines.add(airline);
 	}
 
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void addDepartureAirline(Airline airline) {
+		departureAirlines.add(airline);
 	}
 
 	public Address getAddress() {
@@ -48,9 +40,37 @@ public class Airport {
 		this.address = address;
 	}
 
+
+
+	public String getCode() {
+		return code;
+	}
+
+	public List<Airline> getdepartureAirlines() {
+		return departureAirlines;
+	}
+
+	public boolean isAirlineAllowedToDepart(Airline airline) {
+		for (Airline al : this.departureAirlines) {
+			if (airline.getCode().equals(al.getCode())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean isAirlineAllowedToArrival(Airline airline) {
+		for (Airline al : this.arrivalAirlines) {
+			if (airline.getCode().equals(al.getCode())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	public String toString() {
-		return "Airport [id=" + id + ", code=" + code + ", name=" + name + ", Address=" + address + "]";
+		return "Airport{" + "code='" + code + '\'' + ", name='" + name + '\'' + '}';
 	}
 
 }
