21cc7b8f2e289fa94cc6931b6dd204fc1b319e6c
John Jorjill <orgil9506@gmail.com>
2021-01-19 16:54:38 -0600

    Added abstract methods like findPassengerById(), findAgentById(), getSampleCity() and changed Collection type to List

--- src/main/java/edu/miu/cs/cs401/project/domain/Pilot.java
+++ src/main/java/edu/miu/cs/cs401/project/domain/Pilot.java
@@ -1,5 +1,5 @@
 package edu.miu.cs.cs401.project.domain;
 
 public class Pilot {
-
+	
 }
--- src/main/java/edu/miu/cs/cs401/project/repository/ReservationSystemRepository.java
+++ src/main/java/edu/miu/cs/cs401/project/repository/ReservationSystemRepository.java
@@ -1,7 +1,9 @@
 package edu.miu.cs.cs401.project.repository;
 
 import java.util.Collection;
+import java.util.List;
 
+import edu.miu.cs.cs401.project.domain.Agent;
 import edu.miu.cs.cs401.project.domain.Airline;
 import edu.miu.cs.cs401.project.domain.Airport;
 import edu.miu.cs.cs401.project.domain.Flight;
@@ -10,18 +12,24 @@ import edu.miu.cs.cs401.project.domain.Reservation;
 
 public interface ReservationSystemRepository {
 	
-	Collection<Airport> findAllAirports();
+	List<Airport> findAllAirports();
 	
 	Airport findAirportByAirportCode(String airportCode);
 	
-	Collection<Airport> findAirportsByCity(String city); // Airport(s) of a city, e.g. Chicago has two major airports
+	List<Airport> findAirportsByCity(String city); // Airport(s) of a city, e.g. Chicago has two major airports
 	
-	Collection<Airline> findAirlinesByAirportCode(String airportCode);
+	List<Airline> findAirlinesByAirportCode(String airportCode);
 	
-	Collection<Flight> findFlightsFromTo(String departure, String arrival);
+	List<Flight> findFlightsFromTo(String departure, String arrival);
 	
-	Collection<Reservation> findReservationsByPassengerId(Integer passengerId);
+	List<Reservation> findReservationsByPassengerId(Integer passengerId);
 	
-	Collection<Passenger> findPassengersByAgentCode(String agentCode);
+	List<Passenger> findPassengersByAgentCode(String agentCode);
+	
+	Passenger findPassengerById(int id);
+	
+	Agent findAgentById(int id);
+	
+	String getSampleCity();
 	
 }
